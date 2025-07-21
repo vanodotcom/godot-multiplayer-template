dcd1833e390c3d2edb74dcd6ed91b7ec980cf50f
Vano <vano4real@proton.me>
2025-07-20 23:19:45 -0500

    Update player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())  # makes sure each peer controls its player
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if is_multiplayer_authority():
 		velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 400
 	move_and_slide()
